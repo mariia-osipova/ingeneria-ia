e25d1a7c24a3b30d41fa05b18e8879d0893ca2a8
maria-osipova <maryosipovaa@gmail.com>
2025-05-12 21:29:10 -0300

    tutorial 7

--- economia/economia-tut-7.py
+++ economia/economia-tut-7.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import subprocess
+from pathlib import Path
 
 Q = np.linspace(0, 500, 400)
 
@@ -21,6 +23,16 @@ plt.xlabel('Q', fontsize=14)
 plt.legend(fontsize=12)
 plt.grid(True)
 
-mask = Q <= 250  #P=50, Q1=5*50=250
+#save
+fname = Path("my_plot.png").resolve()
+plt.savefig(fname, dpi=300, bbox_inches="tight")
+
+# copy to memory (macOS) so i can use command + v then
+script = f'''
+set the clipboard to (read (POSIX file "{fname}") as «class PNGf»)
+'''
+subprocess.run(["osascript", "-e", script], check=True)
+
+fname.unlink() #delete the file because i dont fucking need this in my code
 
 plt.show()

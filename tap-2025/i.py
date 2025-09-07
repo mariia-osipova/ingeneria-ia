bb7571ede2e237d9be5a81c93dde4ebbc59f7eba
Mariia Osipova <maryosipovaa@gmail.com>
2025-09-07 17:28:31 -0300

    working version

--- tap-2025/i.py
+++ tap-2025/i.py
@@ -6,17 +6,18 @@ ret = [0] * (n+1)
 for ronda in range(m):
     X, Y = readInts()
     c = list(readInts())
-    c1 = c.count(2)
+    c1 = c.count(1)
     opcionX = X//(c1 +1)
     if opcionX >= Y:
         ret[n] += opcionX
     else:
         ret[n] += Y
-        opcionX = X//(c1 +1)
+        if c1 != 0:
+            opcionX = X//c1
     for i in range(n):
         if c[i] == 2:
             ret[i] += Y
         else:
-            ret[i] += Y
+            ret[i] += opcionX
 
 print(" ".join(map(str, ret)))

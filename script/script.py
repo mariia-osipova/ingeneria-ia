a850f2ccedc5252c86bbd022bca1282d9e1c0405
Mariia Osipova <maryosipovaa@gmail.com>
2025-09-28 11:58:10 -0300

    refactor: reorganize project structure by courses and semester

--- script/script.py
+++ script/script.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-folder_path = "/Users/maria/PycharmProjects/PythonProject/udesa/I100/tutorial"
+folder_path = "/2025S2/I100-PC/tutorial"
 pattern = re.compile(r"(\d{1,2})-(\d{1,2})-(\d{2})(.*)\.py")
 
 for filename in os.listdir(folder_path):
